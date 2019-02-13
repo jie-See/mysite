c16b726f209be14849394fe9649339384a6c99eb
shizhijie <739698292@qq.com>
2019-02-13 18:40:51 +0800

    'second'

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import BlogAtricles
 # Create your views here.
+
+def blog_title(request):
+    blogs = BlogAtricles.objects.all()
+    return render(request, "blog/titles.html", {"blogs": blogs})
